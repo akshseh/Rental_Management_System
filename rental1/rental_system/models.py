d356ff3a80d7770ad1f5c5bb1e91422c73657ca4
Akarsha Sehwag <akarsha15010@iiitd.ac.in>
2017-04-17 01:15:50 +0530

    updated..

--- rental1/rental_system/models.py
+++ rental1/rental_system/models.py
@@ -16,29 +16,39 @@ class Property(models.Model):
 	location = models.CharField(max_length = 50,blank = False,null = False) 
 	num_views = models.IntegerField(default=0)
 	avg_rating = models.IntegerField(default=0)
-	owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+	owner = models.ForeignKey('rental_system.Owner', on_delete=models.CASCADE, null = True)
+	def __str__(self):
+		return str(self.id)
 
 class Visitor(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	id = models.AutoField(primary_key = True)
 	profile = models.TextField(blank = False)
 	pref_location = models.CharField(max_length = 30) 
+	def __str__(self):
+		return self.user.username;
 
 class Owner(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	id = models.AutoField(primary_key = True)
 	num_properties = models.IntegerField(default=0)
 	owner_name = models.CharField(max_length=30)
+	def __str__(self):
+		return self.owner_name
 
 class Rented(models.Model):
 	owner_id = models.ForeignKey('rental_system.Owner',on_delete = models.CASCADE )
 	prop_id = models.ForeignKey('rental_system.Property',on_delete = models.CASCADE )
 	visitor_id = models.ForeignKey('rental_system.Visitor',on_delete = models.CASCADE )
 	rent_to_be_paid = models.IntegerField(default=0)
+	def __str__(self):
+		return "%s %s %s" % (self.owner_id, self.visitor_id, self.rent_to_be_paid)
 
 class Review(models.Model):
 	prop_id = models.ForeignKey('rental_system.Property',on_delete = models.CASCADE )
 	visitor_id = models.ForeignKey('rental_system.Visitor',on_delete = models.CASCADE )
-	visitor_name = models.CharField(max_length = 30, blank = False)
+	# visitor_name = models.CharField(max_length = 30, blank = False)
 	rating = models.IntegerField(default=0)
 	comment = models.CharField(max_length = 100,blank = False)
+	def __str__(self):
+		return "%s %s %s" % (self.comment, self.rating, self.visitor_id)
