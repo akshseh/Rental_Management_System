94ed88aa00febd6ce798750c3b746082353a2185
Akarsha Sehwag <akarsha15010@iiitd.ac.in>
2017-04-17 01:16:28 +0530

    file updated

--- rental1/rental_system/views.py
+++ rental1/rental_system/views.py
@@ -37,77 +37,170 @@ def login_user(request):
 ##########################################################
 @login_required
 def dashboard(request):
-
+    cursor = connection.cursor()
+    current_user = request.user
+    curr_visitor = Visitor.objects.get(user = current_user)
+    cursor.execute("""
+        SELECT T.id, T.description, T.price, T.location, T.num_views
+        FROM rental_system_property as T 
+        WHERE id NOT IN 
+            (SELECT prop_id_id 
+             FROM rental_system_rented)""");#Now it gets linked to the Google API and then gets sorted on the basis of distance
+    prop_list = dictfetchall(cursor)
+    # prop_list = Property.objects.all()
+    paginator = Paginator(prop_list, 2)
+    page = request.GET.get('page')
+    try:
+        property_disp = paginator.page(page)
+    except PageNotAnInteger:
+        property_disp = paginator.page(1)
+    except EmptyPage:
+        property_disp = paginator.page(paginator.num_pages)
+    
     if(request.method == "POST"):
-        cursor = connection.cursor()
         sortBy = 'avg_rating'
-        current_user = request.user
-        visitor = Visitor.obects.get(user = current_user)
-        preferred_location = visitor.pref_location
-        if request.POST['sortBy'] == '1':
-            sortBy = 'avg_rating'
-        # elif request.POST['sortBy'] == '2':
-        #     cursor.execute('SELECT id, location from property');
-        #     prop_list1 = dictfetchall(cursor)
-        #     prop_list = []
-        #     url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="
-        #     for i in prop_list1:
-        #         prop_id = int(i['id'])
-        #         url += i['location']
-        #         url += "&destinations="
-        #         url += preferred_location
-        #         url += "&key=AIzaSyC6h2gHIWNCKwSNJzBNrYoimC068Ma34KA"
-        #         req = urllib.request.Request(url)
-        #         response = urllib.request.urlopen(req)
-        #         json_data = response.read().decode('ascii')
-                # res = json.loads(json_data)
-        #         for index, i in enumerate(res['rows']):
-        #             for j in i['elements']:
-        #                 if j['distance']['value'] > 1000:
-        #                     cursor.execute('SELECT * FROM property WHERE id = %d', [prop_id]);
-        #                     answer = dictfetchall(cursor)
-                            
-        elif request.POST['sortBy'] == '3':
-            sortBy = 'price'
+        print(request.POST)
+        preferred_location = curr_visitor.pref_location
+        if 'sortBy' in request.POST:
+            if request.POST['sortBy'] == '1':
+                sortBy = 'avg_rating'
+            # elif request.POST['sortBy'] == '2':
+            #     cursor.execute('SELECT id, location from property');
+            #     prop_list1 = dictfetchall(cursor)
+            #     prop_list = []
+            #     url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="
+            #     for i in prop_list1:
+            #         prop_id = int(i['id'])
+            #         url += i['location']
+            #         url += "&destinations="
+            #         url += preferred_location
+            #         url += "&key=AIzaSyC6h2gHIWNCKwSNJzBNrYoimC068Ma34KA"
+            #         req = urllib.request.Request(url)
+            #         response = urllib.request.urlopen(req)
+            #         json_data = response.read().decode('ascii')
+                    # res = json.loads(json_data)
+            #         for index, i in enumerate(res['rows']):
+            #             for j in i['elements']:
+            #                 if j['distance']['value'] > 1000:
+            #                     cursor.execute('SELECT * FROM property WHERE id = %d', [prop_id]);
+            #                     answer = dictfetchall(cursor)
+                                
+            elif request.POST['sortBy'] == '3':
+                sortBy = 'price'
 
 
+        if 'query' in request.POST:
+            if request.POST['query'] == '1':
+                x = int(request.POST['val'])
+                if 'sortBy' in request.POST:
+                    if request.POST['sortBy'] == '1':
+                        cursor.execute("""
+                            SELECT T.id, T.description, T.price, T.location, T.num_views
+                            FROM(SELECT * 
+                                 FROM rental_system_property 
+                                 WHERE id NOT IN 
+                                    (SELECT prop_id_id 
+                                     FROM rental_system_rented)) AS T NATURAL JOIN rental_system_review AS R
+                            WHERE id IN 
+                                (SELECT prop_id_id 
+                                 FROM rental_system_review 
+                                 GROUP BY prop_id_id 
+                                 HAVING AVG(rating) >= %s)
+                            GROUP BY T.id
+                            ORDER BY AVG(R.rating) DESC""", [x]);
+                    else:
+                        cursor.execute("""
+                            SELECT T.id, T.description, T.price, T.location, T.num_views
+                            FROM(SELECT * 
+                                 FROM rental_system_property 
+                                 WHERE id NOT IN 
+                                    (SELECT prop_id_id 
+                                     FROM rental_system_rented)) AS T
+                            WHERE id IN 
+                                (SELECT prop_id_id 
+                                 FROM rental_system_review 
+                                 GROUP BY prop_id_id 
+                                 HAVING AVG(rating) >= %s)
+                            ORDER BY T.price""", [x])
+                prop_list = dictfetchall(cursor)
+            elif request.POST['query'] == '2':
+                print("query 1 being executed")
+                x = int(request.POST['val'])
+                if 'sortBy' in request.POST:
+                    if(request.POST['sortBy'] == '1'):
+                        cursor.execute("""
+                            SELECT T.id, T.description, T.price, T.location, T.num_views
+                            FROM(SELECT * 
+                                 FROM rental_system_property 
+                                 WHERE id NOT IN 
+                                    (SELECT prop_id_id 
+                                     FROM rental_system_rented)) AS T NATURAL JOIN rental_system_review as R
+                            WHERE id IN (SELECT prop_id_id 
+                                        FROM rental_system_review 
+                                        WHERE rating = 5 
+                                        GROUP BY prop_id_id 
+                                        HAVING COUNT(rating)>=%s)
+                            GROUP BY R.prop_id_id
+                            ORDER BY AVG(R.rating) DESC""", [x]);
+                    else:
+                        cursor.execute("""
+                        SELECT T.id, T.description, T.price, T.location, T.num_views
+                        FROM(SELECT *
+                             FROM rental_system_property 
+                             WHERE id NOT IN 
+                                (SELECT prop_id_id 
+                                 FROM rental_system_rented)) AS T 
+                        WHERE id IN (SELECT prop_id_id 
+                                    FROM rental_system_review 
+                                    WHERE rating = 5 
+                                    GROUP BY prop_id_id 
+                                    HAVING COUNT(rating)>=%s)
+                        ORDER BY T.price""", [x]);
+                prop_list = dictfetchall(cursor)
+            elif request.POST['query'] == '3':
+                x = int(request.POST['val'])
+                cursor.execute('SELECT id, location FROM property')#Now it gets linked to the Google API and then gets sorted on the basis of distance
+                prop_list = dictfetchall(cursor)
+            elif request.POST['query'] == '4':
+                x = int(request.POST['val'])
+                if 'sortBy' in request.POST:
+                    if request.POST['sortBy'] == 1:
+                        cursor.execute("""
+                            SELECT T.id, T.description, T.price, T.location, T.num_views
+                            FROM(SELECT * 
+                                 FROM rental_system_property 
+                                 WHERE id NOT IN 
+                                    (SELECT prop_id_id 
+                                     FROM rental_system_rented)) AS T NATURAL JOIN rental_system_review AS R 
+                            WHERE price <= %s
+                            GROUP BY R.prop_id_id 
+                            ORDER BY AVG(R.rating) DESC""", [x])
+                    else:
+                        cursor.execute("""
+                            SELECT T.id, T.description, T.price, T.location, T.num_views
+                            FROM(SELECT * 
+                                 FROM rental_system_property 
+                                 WHERE id NOT IN 
+                                    (SELECT prop_id_id 
+                                     FROM rental_system_rented)) AS T 
+                            WHERE price <= %s
+                            ORDER BY T.price""", [x])
+                prop_list = dictfetchall(cursor)
 
-        if request.POST['query'] == '1':
-            x = float(request.POST['val'])
-            cursor.execute('SELECT * FROM property WHERE avg_rating >= %f ORDER BY %s', [x, sortBy]);
-            prop_list = dictfetchall(cursor)
-        elif request.POST['query'] == '2':
-            x = int(request.POST['val'])
-            cursor.execute('SELECT prop_id, location, num_views, description, price, avg_rating FROM property NATURAL JOIN (SELECT prop_id, COUNT(*) as c FROM review WHERE rating = 5 GROUP BY prop_id) WHERE c >= %d ORDER BY %s', [x, sortBy]);
-            prop_list = dictfetchall(cursor)
-        elif request.POST['query'] == '3':
-            x = int(request.POST['val'])
-            cursor.execute('SELECT id, location FROM property')
-            prop_list = dictfetchall(cursor)
-        elif request.POST['query'] == '4':
-            x = int(request.POST['val'])
-            cursor.execute('SELECT * FROM property WHERE price <= %f ORDER BY %s' [x, sortBy])
-            prop_list = dictfetchall(cursor)
-        # sort_by = request.POST['sortBy']
-        # more_than_rating = request.POST['rating']
-        # min_x_5_rating = request.POST['minRating']
-        # distance_
-        # freq_occupied = request.POST['freqOccupied']
-        # distance = request.POST['distance']
-
-        #queries implement  
-    ##########################################
-        # prop_list = Property.objects.all()
-        paginator = Paginator(prop_list, 10)
-        page = request.GET.get('page')
-        try:
-            property_disp = paginator.page(page)
-        except PageNotAnInteger:
-            property_disp = paginator.page(1)
-        except EmptyPage:
-            property_disp = paginator.page(paginator.num_pages)
-        
-    return render(request, "visitor_dashboard.html", {})
+            paginator = Paginator(prop_list, 2)
+            page = request.GET.get('page')
+            try:
+                property_disp = paginator.page(page)
+            except PageNotAnInteger:
+                property_disp = paginator.page(1)
+            except EmptyPage:
+                property_disp = paginator.page(paginator.num_pages)
+            
+    context = {
+        'curr_visitor':curr_visitor,
+        'properties': property_disp
+    }
+    return render(request, "visitor_dashboard.html", context)
 
 ##########################################################
 @login_required
@@ -117,10 +210,32 @@ def owner_dashboard(request):
     #       pass    #
     #   if(request.POST['rent_pending']):
     #       pass#
+    context = {}
     current_user = request.user
+    #basically these are SQL QUERIES !!
     current_owner = Owner.objects.get(user = current_user)
-
-    return render(request, "owner_dashboard.html",{})
+    tenants = Rented.objects.filter(owner_id = current_owner)
+    print("I have these tenants - ", tenants)
+    properties = Property.objects.filter(owner = current_owner)
+    print(properties)
+    if(request.method == "POST"):
+        if 'sort' in request.POST:
+            if request.POST['sort'] == '1':
+                tenants = tenants.order_by('visitor_id')
+            elif request.POST['sort'] == '2':
+                tenants = tenants.order_by('rent_to_be_paid')
+    paginator = Paginator(properties, 2)
+    page = request.GET.get('page')
+    try:
+        property_disp = paginator.page(page)
+    except PageNotAnInteger:
+        property_disp = paginator.page(1)
+    except EmptyPage:
+        property_disp = paginator.page(paginator.num_pages)
+        
+    print(current_owner.owner_name)
+    context = {'tenants':tenants,'properties': property_disp}
+    return render(request, "owner_dashboard.html",context)
 
 ##########################################################
 
@@ -145,6 +260,32 @@ def register(request):
 
 
 ##########################################################
+
+def register_visitor(request):
+    error = None
+    if (request.method =="POST"):
+        username = request.POST['username']
+        password = request.POST['password']
+        # name = request.POST['name']
+        profile = request.POST['profile']
+        pref_location = request.POST['pref_location']
+        existing = User.objects.filter(username__iexact=username)
+        if existing.exists():
+            error = "This username already exists. Kindly provide another username."
+        
+        new_user = User.objects.create_user(username = username, password = password, is_owner = False)
+        new_user.save()
+        new_visitor = Visitor.objects.create(user = new_user, profile = profile, pref_location = pref_location)
+        print(new_user)
+        # new_owner.user = new_user
+        new_visitor.save()
+        return redirect('login')
+    return render(request, "register_visitor.html",{'error':error})
+
+
+def view_prop(request):
+    return render(request, "view_property.html", {})
+
 @login_required
 def add_property(request):
     # if request.method == "POST":
